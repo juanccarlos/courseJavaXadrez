72d1b572557559669639b39c481bda8f2c653932
juanccarlos <juannnerysantos@gmail.com>
2022-09-07 10:14:31 -0300

    Pawn and Bishop

--- src/application/UI.java
+++ src/application/UI.java
@@ -35,42 +35,39 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	public static void clearScreen() {
-		 System.out.print("\033[H\033[2J");
-		 System.out.flush();
-		}
-	
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
-	try {
+		try {
 			String s = sc.nextLine();
 			char cloumn = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
-			return new ChessPosition(cloumn, row);	
+			return new ChessPosition(cloumn, row);
 		}
-		catch (RuntimeException e){
+		catch (RuntimeException e) {
 			throw new InputMismatchException("\"Error reading ChessPosition. Valid values are from a1 to h8 ");
 		}
 	}
-	
+
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn :" + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getcurrentPlayer());
-			if (!chessMatch.getCheckMate()) {
-				System.out.println("Waiting player: " + chessMatch.getcurrentPlayer());
-				if (chessMatch.getCheck()) {
-					System.out.println(ANSI_GREEN + "CHECK!" + ANSI_RESET);
-				}
-			}
-			else {
-				System.out.println(ANSI_GREEN + "CHECKMATE!" + ANSI_RESET);
-				System.out.println("Winner: " + chessMatch.getcurrentPlayer());
+		System.out.println("Turn : " + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getcurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
 			}
 		}
-
-	
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getcurrentPlayer());
+		}
+	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
@@ -93,9 +90,9 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	private static void printPiece(ChessPiece piece, boolean background) {
-		if(background) {
+		if (background) {
 			System.out.print(ANSI_YELLOW_BACKGROUND);
 		}
 		if (piece == null) {
@@ -109,10 +106,10 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> red = captured.stream().filter(x -> x.getColor()== Color.RED).collect(Collectors.toList());
-		List<ChessPiece> blue = captured.stream().filter(x -> x.getColor()== Color.BLUE).collect(Collectors.toList());
+		List<ChessPiece> red = captured.stream().filter(x -> x.getColor() == Color.RED).collect(Collectors.toList());
+		List<ChessPiece> blue = captured.stream().filter(x -> x.getColor() == Color.BLUE).collect(Collectors.toList());
 		System.out.println("captured pieces:");
 		System.out.print("red: ");
 		System.out.print(ANSI_RED);
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,13 +2,14 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -84,8 +85,6 @@ public class ChessMatch {
 		else {
 			nextTurn();
 		}
-		
-		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
 
@@ -196,12 +195,33 @@ public class ChessMatch {
 	}	
 	
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.RED));
-		placeNewPiece('d', 1, new Rook(board, Color.RED));
-		placeNewPiece('e', 1, new King(board, Color.RED));
+		placeNewPiece('a', 1, new Rook(board, Color.RED));
+		placeNewPiece('c', 1, new Bishop(board, Color.RED));
+        placeNewPiece('e', 1, new King(board, Color.RED));
+        placeNewPiece('f', 1, new Bishop(board, Color.RED));
+        placeNewPiece('h', 1, new Rook(board, Color.RED));
+        placeNewPiece('a', 2, new Pawn(board, Color.RED));
+        placeNewPiece('b', 2, new Pawn(board, Color.RED));
+        placeNewPiece('c', 2, new Pawn(board, Color.RED));
+        placeNewPiece('d', 2, new Pawn(board, Color.RED));
+        placeNewPiece('e', 2, new Pawn(board, Color.RED));
+        placeNewPiece('f', 2, new Pawn(board, Color.RED));
+        placeNewPiece('g', 2, new Pawn(board, Color.RED));
+        placeNewPiece('h', 2, new Pawn(board, Color.RED));
 		
 
-		placeNewPiece('b', 8, new Rook(board, Color.BLUE));
-		placeNewPiece('a', 8, new King(board, Color.BLUE));
+        placeNewPiece('a', 8, new Rook(board, Color.BLUE));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLUE));
+        placeNewPiece('e', 8, new King(board, Color.BLUE));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLUE));
+        placeNewPiece('h', 8, new Rook(board, Color.BLUE));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLUE));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLUE));
 	}
 }
